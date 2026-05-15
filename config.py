7e1eefaa225f64b51b0d0b44388d89a445138588
VikasSmetsPXL <vikas.smets@student.pxl.be>
2026-05-15 13:31:42 +0200

    Enhance config.py with additional comments
    Updated router configuration with comments for clarity.

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # config.py
-ROUTER_HOST = "192.168.56.103"
+# config.py
+ROUTER_HOST = "192.168.56.103"   # Fysieke router: ander IP invullen
 ROUTER_PORT = 830
 ROUTER_USER = "admin"
 ROUTER_PASS = "pxl"
+VIRTUAL_ROUTER = True            # Zet op False voor fysieke router
